5d84c5236cd4b0fa57367de5e2bb92ae02e4d7f2
anna-rurak <annarurakk@gmail.com>
2024-02-07 12:26:03 +0100

    adding js logic

--- index.js
+++ index.js
@@ -2,3 +2,41 @@
 // - See hover states for all interactive elements on the page
 // - Select and submit a number rating
 // - See the "Thank you" card state after submitting a rating
+
+function toggleCards() {
+    $("#rating").hide();
+    $("#thank-you").show();
+}
+
+$(document).ready(function () {
+    $("#rating").show();
+    $("#thank-you").hide();
+});
+
+$(".point").on("click", function () {
+    $(".point").removeClass("clicked");
+    $(this).toggleClass("clicked");
+});
+
+$("button").on("click", function () {
+    // checking if the score was choosen
+    const scoreArray = $(".point");
+    const hasClickedClass = scoreArray.hasClass("clicked");
+
+    while (true) {
+        if (hasClickedClass === true) {
+            $("#rating").animate({ opacity: 0 }, 500, function () {
+                $("#rating").hide();
+                $("#thank-you").animate({ opacity: 1 }, 700).show();
+            });
+            break;
+        } else {
+            alert("You have to chose the score :)");
+            break;
+        }
+    }
+});
+
+//change speed of the animation
+// add sohwing how many points someone has chosen
+// commit, push
